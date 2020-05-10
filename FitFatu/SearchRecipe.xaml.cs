4e524f61f5d7e17103056193aa0b7c9f8c824923
hubert-coder <61919571+hubert-coder@users.noreply.github.com>
2020-05-10 23:39:14 +0200

    Add files via upload
    Pliki do programu

---
+++ FitFatu/SearchRecipe.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FitFatu
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SearchRecipt : ContentPage
+    {
+        public SearchRecipt()
+        {
+            InitializeComponent();
+        }
+
+        private async void PowrotMenuGosc_ClickedAsync(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new ZalogowanyGosc());
+        }
+    }
+}
